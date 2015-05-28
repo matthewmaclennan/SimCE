aefd7c25f8b7c9fb3ea5e8b53e7042a2b60099a9
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-28 12:24:14 -0700

    Update SimCEmeshMatRearr.R

--- SimCEmeshMatRearr.R
+++ SimCEmeshMatRearr.R
@@ -1,7 +1,9 @@
 #Rearranging the SimCEmeshMatrix to as a function of a single variable
 #Colnames are: 
 SimCEmeshMatRearr<-function(data,colname){
+#Find the time frequencies (i.e. lengths) from the data, display in table, cbind time with frequency
   table<-as.matrix(table(data[,2]))
-  table.bound<-cbind(as.numeric(rownames(table)),table)
-
+  table.b<-cbind(as.numeric(rownames(table)),table)
+  rownames(table.b)<-NULL
+  
 }

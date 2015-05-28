0d0155e8da1e0b0ffe84cc8d6b9aa4949696164d
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-28 12:00:33 -0700

    Update and rename SimCE3mesh.R to SimCE3meshMatrix.R

--- SimCE3meshMatrix.R
+++ SimCE3meshMatrix.R
@@ -1,12 +1,8 @@
 #Creates a numeric matrix from an imported data frame of the *.asc mesh file(s) created by SimCE3.
 #
-SimCE3matrix<-function(filename){
+SimCE3meshMatrix<-function(filename,colname){
 
+#import asc file/filechunk into R
 big1<-as.matrix(read.table(filename,header=T,
 colClasses=c("numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric","numeric")))
-
-  for(i in 1:nrow(big1)){
-
-
-  }
 }

3324ae75be2729330bfd7c8b035313879b6ba17d
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-28 12:21:58 -0700

    Update SimCEmeshMatRearr.R

--- SimCEmeshMatRearr.R
+++ SimCEmeshMatRearr.R
@@ -1,6 +1,7 @@
 #Rearranging the SimCEmeshMatrix to as a function of a single variable
 #Colnames are: 
 SimCEmeshMatRearr<-function(data,colname){
-  
+  table<-as.matrix(table(data[,2]))
+  table.bound<-cbind(as.numeric(rownames(table)),table)
 
 }

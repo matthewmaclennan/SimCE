4f34891034143ddd0c30f35df49ec51284a8d5b2
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-06-03 21:50:17 -0700

    Update eg1_PCA_A-1.par.R

--- eg1_PCA_A-1.par.R
+++ eg1_PCA_A-1.par.R
@@ -1,4 +1,6 @@
 eg1_PCA_A-1.par<-as.matrix(c(64.5,54.3,50,10000,2.04,10.2,20,))
 rownames(eg1_PCA_A-1.par)<-c("Length of Capillary (cm)","Length to Detector (cm)","Inner Diameter (um)",
 "Voltage (V)", "Idle Time (s)", "Ramp Time (s)","Temperature (C)","[A] in BGE (M)","[P] in BGE (M)","[A] in Sample 1 (M)",
-"[P] in Sample 1 (M)",
+"[P] in Sample 1 (M)","Sample 1 Plug Length (cm)","[A] in Sample 2 (M)","[P] in Sample 2 (M)","Sample 2 Plug Length (cm)",
+"Distance Between (cm)","Coefficient of A","Coefficient of P","Coefficient of C","Binding Constant","Association Rate (/s/M...)",
+"Dissociation Rate (/s ...)","Equilibrium or Kinetic",

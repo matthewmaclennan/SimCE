ad5cd4c19e79b5f392f62c49a0bb8c2eede018cd
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-06-03 22:26:05 -0700

    Update eg1_PCA_A-1.par.R

--- eg1_PCA_A-1.par.R
+++ eg1_PCA_A-1.par.R
@@ -1,10 +1,10 @@
-eg1_PCA_A-1.par<-as.matrix(c(64.5,54.3,50,10000,2.04,10.2,20,0,0.005,0.002,0,0.18,0,0.003,0,0,))
+eg1_PCA_A_1.par<-as.matrix(c(64.5,54.3,50,10000,2.04,10.2,20,0,0.005,0.002,0,0.18,0,0.003,0,0,1,1,1,
+533,0,0,0.0001364,0.0003699,0.0002994,0,0,1385.88,1,1,1,1e-6,1e-6,1e-6,0,0,0,0,0,0,0,0))
 r.names<-c("Instrument_Length of Capillary (cm)","Instrument_Length to Detector (cm)","Instrument_Inner Diameter (um)",
 "Instrument_Voltage (V)", "Instrument_Idle Time (s)", "Instrument_Ramp Time (s)","Instrument_Temperature (C)","Chem_[A] in BGE (M)","Chem_[P] in BGE (M)","Chem_[A] in Sample 1 (M)",
 "Chem_[P] in Sample 1 (M)","Chem_Sample 1 Plug Length (cm)","Chem_[A] in Sample 2 (M)","Chem_[P] in Sample 2 (M)","Chem_Sample 2 Plug Length (cm)",
 "Chem_Distance Between (cm)","Stoich_Coefficient of A","Stoich_Coefficient of P","Stoich_Coefficient of C","Binding Constant","Association Rate (/s/M...)",
-"Dissociation Rate (/s ...)","Equilibrium or Kinetic","mu_Free A","mu_Free P","mu_Complex C","mu_Electroosmotic Flow (EOF)","mu_Time of EOF Marker (s)",
+"Dissociation Rate (/s ...)","mu_Free A","mu_Free P","mu_Complex C","mu_Electroosmotic Flow (EOF)","mu_Time of EOF Marker (s)",
 "Migration Time of A (s)","mu_Viscosity Correction A","mu_Viscosity Correction P","mu_Viscosity Correction C","D_Species A","D_Species P","D_Complex C",
-"Wall_[W] (mol/dm^2)","Wall_[W] Used in Calculation","Wall_Delay After Injection","Wall_Pre-equilibrated Wall","WA_Interaction",
-"WA_Adsorption Rate","WA_Desorption Rate","WP_Interaction","WP_Adsorption Rate","WP_Desorption Rate","WC_Interaction",
-"WC_Adsorption Rate","WC_Desorption Rate")
+"Wall_[W] (mol/dm^2)","Wall_Delay After Injection","WA_Adsorption Rate","WA_Desorption Rate","WP_Adsorption Rate",
+"WP_Desorption Rate","WC_Adsorption Rate","WC_Desorption Rate")

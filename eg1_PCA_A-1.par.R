fdd7f6acb24ed9cf527376a6a5711a0f35f915ed
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-06-03 21:46:15 -0700

    Create eg1_PCA_A-1.par.R

---
+++ eg1_PCA_A-1.par.R
@@ -0,0 +1,4 @@
+eg1_PCA_A-1.par<-as.matrix(c(64.5,54.3,50,10000,2.04,10.2,20,))
+rownames(eg1_PCA_A-1.par)<-c("Length of Capillary (cm)","Length to Detector (cm)","Inner Diameter (um)",
+"Voltage (V)", "Idle Time (s)", "Ramp Time (s)","Temperature (C)","[A] in BGE (M)","[P] in BGE (M)","[A] in Sample 1 (M)",
+"[P] in Sample 1 (M)",

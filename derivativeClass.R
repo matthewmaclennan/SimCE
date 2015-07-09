8290c85bafc4dd357c933ca4fe6dddeda8547733
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-07-09 12:36:01 -0700

    Update derivativeClass.R

--- derivativeClass.R
+++ derivativeClass.R
@@ -1,6 +1,7 @@
 #Finds 1st derivative of time-series data derived from SimCEmeshMatRearr and assigns "front" and "plateau" to segments 
 #in the data
-derivativeClass<-function(){
-
+derivativeClass<-function(data){
+data<-as.matrix(data)
+ddata_dt<-diff(data)
 
 }
